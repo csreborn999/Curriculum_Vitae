2edfe112c2bce7f1f3459cd629768a9149a92160
殷振祐 <fpsstargazer@gmail.com>
2019-11-10 18:36:31 +0800

    Update Leetcode_72.cs

--- algorithm/Leetcode_72.cs
+++ algorithm/Leetcode_72.cs
@@ -2,6 +2,24 @@
 using System.Collections.Generic;
 using System.Text;
 
+/*************************
+ * 
+ * Given two words word1 and word2, find the minimum number of operations required to convert word1 to word2.
+ * You have the following 3 operations permitted on a word:
+ * 1.Insert a character
+ * 2.Delete a character
+ * 3.Replace a character
+ *
+ * Example 1:
+ *
+ * Input: word1 = "horse", word2 = "ros"
+ * Output: 3
+ * Explanation: 
+ * horse -> rorse (replace 'h' with 'r')
+ * rorse -> rose (remove 'r')
+ * rose -> ros (remove 'e')
+ *************************/
+
 namespace Leetcode_19_11_06
 {
     class Leetcode_72
