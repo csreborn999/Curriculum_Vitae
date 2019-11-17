fb7df2aaed096dcb56b75b6d66ee206f6e743a5f
殷振祐 <fpsstargazer@gmail.com>
2019-11-18 01:14:49 +0800

    Add files via upload

---
+++ algorithm/Leetcode_476.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+/********************
+ * 
+ * Given a positive integer, output its complement number. The complement strategy is to flip the bits of its binary representation.
+ * Note:
+ * The given integer is guaranteed to fit within the range of a 32-bit signed integer.
+ * You could assume no leading zero bit in the integer’s binary representation.
+ * 
+ * Example 1:
+ * Input: 5
+ * Output: 2
+ * Explanation: The binary representation of 5 is 101 (no leading zero bits), and its complement is 010. So you need to output 2.
+ * 
+ **********************/
+
+namespace Leetcode_19_11_06
+{
+    class Leetcode_476
+    {
+        public int findComplement(int num)
+        {
+            int convert = 1, temp = num / 2;
+            while (temp > 0)
+            {
+                temp /= 2;
+                convert = convert * 2 + 1;
+            }
+            return num ^ convert;
+        }
+
+        static void Main(string[] args)
+        {
+            int num = 20;
+            Console.WriteLine(new Program().findComplement(num));
+        }
+    }
+}
---
+++ algorithm/Leetcode_476.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+/********************
+ * 
+ * Given a positive integer, output its complement number. The complement strategy is to flip the bits of its binary representation.
+ * Note:
+ * The given integer is guaranteed to fit within the range of a 32-bit signed integer.
+ * You could assume no leading zero bit in the integer’s binary representation.
+ * 
+ * Example 1:
+ * Input: 5
+ * Output: 2
+ * Explanation: The binary representation of 5 is 101 (no leading zero bits), and its complement is 010. So you need to output 2.
+ * 
+ **********************/
+
+public class Leetcode_476 {
+	public int findComplement(int num) {
+		int convert = 1, temp = num/2;
+		while (temp>0) {
+			temp /=2;
+			convert = convert*2+1;
+		}
+		return num^convert;
+    }
+
+    public static void main(String[] args) {
+    	int num = 20;
+    	System.out.println(new Leetcode_476().findComplement(num));
+    }
+}
